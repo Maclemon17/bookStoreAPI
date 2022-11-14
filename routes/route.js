76f4be300ac856d81b9f50ad3739593d9538e800
Maclemon17 <maclemon17@gmail.com>
2022-11-14 12:55:06 +0100

    route name fixed

--- routes/route.js
+++ routes/route.js
@@ -4,11 +4,11 @@ const { addBook, getAllBooks, getBook, updateBook, deleteBook } = require("../co
 
 const router = express.Router();
 
-router.post("/book", addBook);              // ADD ANEW BOOK
+router.post("/books", addBook);              // ADD ANEW BOOK
 router.get('/books', getAllBooks);          // GET ALL BOOKS
-router.get('/book/:id', getBook);           // GET A SINGLE BOOK
-router.put('/book/:id', updateBook);        // UPDATE A SINGLE BOOK
-router.delete('/book/:id', deleteBook);     // DELETE A SINGLE BOOK    
+router.get('/books/:id', getBook);           // GET A SINGLE BOOK
+router.put('/books/:id', updateBook);        // UPDATE A SINGLE BOOK
+router.delete('/books/:id', deleteBook);     // DELETE A SINGLE BOOK    
 
 
 
